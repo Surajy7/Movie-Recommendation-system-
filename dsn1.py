c51e6947a0cded4bc5c94c82de3fac05e28a6e94
Surajy7 <130474349+Surajy7@users.noreply.github.com>
2023-04-15 18:11:42 +0530

    Add files via upload

---
+++ dsn1.py
@@ -0,0 +1,101 @@
+# ## Movie Recommendation System
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+from sklearn.metrics.pairwise import cosine_similarity
+from ast import literal_eval
+
+
+# path = "D:\college\project\metabase"
+credits_df = pd.read_csv("tmdb_5000_credits.csv")
+movies_df = pd.read_csv("tmdb_5000_movies.csv")
+
+movies_df.head()
+credits_df.head()
+
+credits_df.columns = ['id','tittle','cast','crew']
+movies_df = movies_df.merge(credits_df, on="id")
+
+movies_df.head() # new dataframe
+
+# Demographic Filtering
+C = movies_df["vote_average"].mean()
+m = movies_df["vote_count"].quantile(0.9)
+
+print("C: ", C)
+print("m: ", m)
+
+new_movies_df = movies_df.copy().loc[movies_df["vote_count"] >= m]
+print(new_movies_df.shape)
+
+def weighted_rating(x, C=C, m=m):
+    v = x["vote_count"]
+    R = x["vote_average"]
+
+    return (v/(v + m) * R) + (m/(v + m) * C)
+
+
+new_movies_df["score"] = new_movies_df.apply(weighted_rating, axis=1)
+new_movies_df = new_movies_df.sort_values('score', ascending=False)
+
+new_movies_df[["title", "vote_count", "vote_average", "score"]].head(10)
+
+# Plot top 10 movies
+def plot():
+    popularity = movies_df.sort_values("popularity", ascending=False)
+    plt.figure(figsize=(12, 6))
+    plt.barh(popularity["title"].head(10), popularity["popularity"].head(10), align="center", color="skyblue")
+    plt.gca().invert_yaxis()
+    plt.title("Top 10 movies")
+    plt.xlabel("Popularity")
+    plt.show()
+    
+plot()
+
+# Content based Filtering
+print(movies_df["overview"].head(5))
+
+
+tfidf = TfidfVectorizer(stop_words="english")
+movies_df["overview"] = movies_df["overview"].fillna("")
+
+tfidf_matrix = tfidf.fit_transform(movies_df["overview"])
+print(tfidf_matrix.shape)
+
+
+# Compute similarity
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+print(cosine_sim.shape)
+
+indices = pd.Series(movies_df.index, index=movies_df["title"]).drop_duplicates()
+print(indices.head())
+
+
+def get_recommendations(title, cosine_sim=cosine_sim):
+    """
+    in this function,
+        we take the cosine score of given movie
+        sort them based on cosine score (movie_id, cosine_score)
+        take the next 10 values because the first entry is itself
+        get those movie indices
+        map those indices to titles
+        return title list
+    """
+    idx = indices[title]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:6]
+    # (a, b) where a is id of movie, b is sim_score
+
+    movies_indices = [ind[0] for ind in sim_scores]
+    movies = movies_df["title"].iloc[movies_indices]
+    return movies
+
+print("################ Content Based Filtering - plot#############")
+print()
+user_search = input("Enter the Name of the Movie: ")
+print(f"Recommendations for {user_search}")
+print(get_recommendations(user_search.title()))
