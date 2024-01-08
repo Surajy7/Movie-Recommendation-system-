7e3d3ced29ecacadc87070e60d0eb4af39a4a1d3
Suraj Yadav <itssurajyadav7@gmail.com>
2024-01-08 21:36:14 +0530

    Update dsn1.py

--- dsn1.py
+++ dsn1.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import CountVectorizer 
+from sklearn.feature_extraction.text import CountVectorizer  
 from sklearn.metrics.pairwise import linear_kernel
 from sklearn.metrics.pairwise import cosine_similarity
 from ast import literal_eval 
